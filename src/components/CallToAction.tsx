5c69d4b1264f09323e025b8de23e9edf40823f26
santoine@nerdery.com <santoine@nerdery.com>
2023-03-05 12:13:17 -0500

    change font to small

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -38,7 +38,7 @@ const CallToAction: FunctionComponent<Props> = ({
   return (
     <>
       <div className='tw-col-span-6'>
-        <h2 className='tw-font-medium tw-text-base'>
+        <h2 className='tw-font-medium tw-text-sm'>
           Please enter your name and best email address so we can send your
           results, including a printable macronutrient document.{' '}
         </h2>
