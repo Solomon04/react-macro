be429c0788912fd62a4cbed5be1f62589811270a
santoine@nerdery.com <santoine@nerdery.com>
2023-03-10 23:01:26 -0600

    add fixed of 0

--- src/formulas/macros.ts
+++ src/formulas/macros.ts
@@ -1,49 +1,49 @@
 export const balancedFormula = (tdee: number): DietPlan => {
   return {
     name: 'Balanced Diet',
-    calories: Math.round(tdee).toPrecision(1),
-    fat: Math.round((tdee * 0.3) / 9).toPrecision(1),
-    carbs: Math.round((tdee * 0.4) / 4).toPrecision(1),
-    protein: Math.round((tdee * 0.3) / 4).toPrecision(1),
+    calories: Math.round(tdee).toFixed(0),
+    fat: Math.round((tdee * 0.3) / 9).toFixed(0),
+    carbs: Math.round((tdee * 0.4) / 4).toFixed(0),
+    protein: Math.round((tdee * 0.3) / 4).toFixed(0),
   }
 }
 
 export const lowCarbFormula = (tdee: number): DietPlan => {
   return {
     name: 'Low Carb Diet',
-    calories: Math.round(tdee).toPrecision(1),
-    fat: Math.round((tdee * 0.3) / 9).toPrecision(1),
-    carbs: Math.round((tdee * 0.2) / 4).toPrecision(1),
-    protein: Math.round((tdee * 0.5) / 4).toPrecision(1),
+    calories: Math.round(tdee).toFixed(0),
+    fat: Math.round((tdee * 0.3) / 9).toFixed(0),
+    carbs: Math.round((tdee * 0.2) / 4).toFixed(0),
+    protein: Math.round((tdee * 0.5) / 4).toFixed(0),
   }
 }
 
 export const highCarbFormula = (tdee: number): DietPlan => {
   return {
     name: 'High Carb Diet',
-    calories: Math.round(tdee).toPrecision(1),
-    fat: Math.round((tdee * 0.2) / 9).toPrecision(1),
-    carbs: Math.round((tdee * 0.5) / 4).toPrecision(1),
-    protein: Math.round((tdee * 0.3) / 4).toPrecision(1),
+    calories: Math.round(tdee).toFixed(0),
+    fat: Math.round((tdee * 0.2) / 9).toFixed(0),
+    carbs: Math.round((tdee * 0.5) / 4).toFixed(0),
+    protein: Math.round((tdee * 0.3) / 4).toFixed(0),
   }
 }
 
 export const highProteinFormula = (tdee: number): DietPlan => {
   return {
     name: 'High Protein Diet',
-    calories: Math.round(tdee).toPrecision(1),
-    fat: Math.round((tdee * 0.25) / 9).toPrecision(1),
-    carbs: Math.round((tdee * 0.25) / 4).toPrecision(1),
-    protein: Math.round((tdee * 0.5) / 4).toPrecision(1),
+    calories: Math.round(tdee).toFixed(0),
+    fat: Math.round((tdee * 0.25) / 9).toFixed(0),
+    carbs: Math.round((tdee * 0.25) / 4).toFixed(0),
+    protein: Math.round((tdee * 0.5) / 4).toFixed(0),
   }
 }
 
 export const ketoFormula = (tdee: number): DietPlan => {
   return {
     name: 'Keto Diet',
-    calories: Math.round(tdee).toPrecision(1),
-    fat: Math.round((tdee * 0.5) / 9).toPrecision(1),
-    carbs: Math.round((tdee * 0.1) / 4).toPrecision(1),
-    protein: Math.round((tdee * 0.4) / 4).toPrecision(1),
+    calories: Math.round(tdee).toFixed(0),
+    fat: Math.round((tdee * 0.5) / 9).toFixed(0),
+    carbs: Math.round((tdee * 0.1) / 4).toFixed(0),
+    protein: Math.round((tdee * 0.4) / 4).toFixed(0),
   }
 }
