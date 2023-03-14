bb305b642f52a3b6fd40666704ed038751b929a2
santoine@nerdery.com <santoine@nerdery.com>
2023-03-14 00:20:49 -0500

    remove preflight styles for specific tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,29 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('flowbite/plugin'), require('@tailwindcss/typography')],
-  prefix: 'tw-'
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        // Enable default browser styles for h1-h6, p, li, and ol
+        'h1, h2, h3, h4, h5, h6, p, li, ol': {
+          margin: 0,
+          padding: 0,
+          fontWeight: 'inherit',
+          fontSize: 'inherit',
+          lineHeight: 'inherit',
+          listStyleType: 'inherit',
+          fontFamily: 'inherit',
+          color: 'inherit',
+        },
+      })
+    },
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+    false,
+  ],
+  prefix: 'tw-',
+  corePlugins: {
+    preflight: false,
+  },
 }
