1038e83d37567d4bbe5025dbf7a5eb822f39bcd0
Solomon04 <antoinesolomon5@gmail.com>
2023-03-31 14:10:42 -0400

    console out root element

--- src/App.tsx
+++ src/App.tsx
@@ -139,6 +139,9 @@ export default function App() {
   useEffect(() => {
     const final = Date.now()
     console.log({ final })
+
+    const rootElement = document.getElementById('root')
+    console.log(rootElement)
   })
 
   return (
